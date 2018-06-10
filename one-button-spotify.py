a58b6e2b308fee0bf975314481f75dd88cf31e01
Thomas Bishop <thomasbishop@MacBookAir.local>
2018-06-10 13:44:40 +0200

    fixes

--- one-button-spotify.py
+++ one-button-spotify.py
@@ -26,13 +26,15 @@ device = ''
 scope = 'user-library-read, user-read-playback-state, user-modify-playback-state'
 
 def spotStart():
+    print 'spotStart called'
     global token
     token = util.prompt_for_user_token(username, scope,client_id=SP_CLIENT_ID,client_secret=SP_CLIENT_SECRET,redirect_uri=SP_REDIRECT_URI)
+    print 'token created as ' + token
 
 def spotDevices():
+    print 'spotDevices called'
     global token
-    #print "Get Devices"
-    if token:
+    try:
         global device
         sp = spotipy.Spotify(auth=token)
         devices = sp.devices()
@@ -40,55 +42,70 @@ def spotDevices():
         dictionary = {}
         for item in devices:
             dictionary[item['name']] = item['id']
-        #print dictionary
         device = dictionary[spotconnect_device_name]
-    else:
-        #print "Can't get token for", username
+        print 'device resolved as ' + device
+    except:
+        # empty token
+        print 'something is not right - emptying token'
         token = ''
 
 def spotPlay():
+    print 'spotPlay called'
     global token
     global playing
-    #print "Play"
-    if token:
+    try:
         if playing:
+            print 'already playing - skip'
             # if we're already playing, skip to a new track
             sp = spotipy.Spotify(auth=token)
             sp.next_track()
             time.sleep(1)
         else:
             # if we're not playing, play the playlist, turn on shuffle and skip to a new (random) track
+            print 'not playing - trying to start'
             sp = spotipy.Spotify(auth=token)
             sp.start_playback(device_id=device,context_uri=playlist)
             sp.shuffle(True)
             sp.next_track()
             playing = True
             time.sleep(1)
-    else:
-        #print "Can't get token for", username
+    except:
+        # empty token
+        print 'something is not right - emptying token'
         token = ''
 
 def spotStop():
+    print 'spotStop called'
     global token
     global playing
-    #print "Stop"
-    if token:
+    try:
         if playing:
             # stop(pause) playing
+            print 'currently playing - trying to stop'
             sp = spotipy.client.Spotify(auth=token)
             sp.pause_playback()
             playing = False
             time.sleep(1)
         else:
             # if not playing, do nothing
+            print 'not playing - doing nothing'
             pass
-    else:
-        #print "Can't get token for", username
+    except:
+        # empty token
+        print 'something is not right - emptying token'
         token = ''
+
+idle = 0
 
 while True:
     if not token:
         spotStart()
         spotDevices()
+    if idle == 14400: # roughly every hour (14400*0.25secs = 60 mins) empty token
+        idle = 0
+        token = ''
+    else:
+        idle += 1
+        time.sleep(0.25)
     button.when_pressed = spotPlay
     button.when_held = spotStop
